42345a84d8524175f43a0ee7772c7e9c05df2a26
dxAnddy <arkatnur0212@gmail.com>
2026-02-02 21:50:00 +0500

    added headers for parser and container

---
+++ hw3/rq/include/container.hpp
@@ -0,0 +1,10 @@
+namespace range_queries {
+
+template <typename T>
+class Container {
+    virtual void insert(const T& value) = 0;
+    virtual int count_in_range(const T& left, const T& right) const = 0;
+    virtual ~Container() = default;
+};
+
+}
--- hw3/rq/include/parser.hpp
+++ hw3/rq/include/parser.hpp
@@ -1,44 +1,20 @@
 #pragma once
 
-#include <variant>
-#include <utility>
-#include <stdexcept>
+#include "optypes.hpp"
+#include "container.hpp"
+#include <iostream>
+#include <vector>
 
 namespace range_queries {
 
-using Key = int;
-using Range = std::pair<int, int>;
-
-class QueryOp {
+class Parser {
 private:
-    int left_;
-    int right_;
-    void validate(int l, int r) {
-        if(l >= r) throw std::invalid_argument("Invalid range: left >= right");
-    }
+    template <typename InsertFunc, typename QueryFunc>
+    static void parse_and_execute(std::istream &input, InsertFunc insert, QueryFunc query);
 public:
-    explicit QueryOp(int l, int r) : left_(l), right_(r) {
-        validate(l, r);
-    }
-
-    int left() const {return left_;}
-    int right() const {return right_;}
-
-    void set_range(int l, int r) {
-        validate(l, r);
-        left_ = l;
-        right_ = r;
-    }
+    template <typename T>
+    static std::vector<int> process(std::istream &input, Container<T> &con);
 };
 
-class InsertOp {
-private:
-    Key key_;
-public:
-    explicit InsertOp(Key k) : key_(k) {}
-    Key key() const { return key_;}
-    void set_key(Key k) {key_ = k;}
-};
-
-using Operation = std::variant<InsertOp, QueryOp>;
 }
+
