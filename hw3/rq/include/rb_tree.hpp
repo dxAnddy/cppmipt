1832b9ccee2889d1af45fa089a887275927832df
dxAnddy <arkatnur0212@gmail.com>
2026-03-26 00:45:18 +0300

    added basic private methods

--- hw3/rq/include/rb_tree.hpp
+++ hw3/rq/include/rb_tree.hpp
@@ -13,8 +13,8 @@ private:
     struct Node {
         T value;
         Color color;
-        std::unique_ptr<Node> left;
-        std::unique_ptr<Node> right;
+        Node *left;
+        Node *right;
         Node *parent;
 
         explicit Node(const T &val) :
@@ -27,8 +27,38 @@ private:
         Node &operator=(Node &&) = default;
     };
     
-    std::unique_ptr<Node> root_;
+    Node *root_;
+
+    static Color get_color(const Node *node) noexcept {
+        return node ? node->color : Color::BLACK;
+    }
+
+    static void set_color(const Node *node, Color color) noexcept {
+        if(node) node->color = color;
+    }
+
+    static Node* get_parent(const Node *node) noexcept {
+        return node ? node->parent : nullptr; 
+    }
+
+    static Node* get_grand_parent(const Node *node) noexcept {
+        return node && node->parent ? node->parent->parent : nullptr;
+    }
+
+    static Node* get_uncle(const Node *node) noexcept {
+        Node *parent = get_parent(node);
+        Node *grand_parent = get_grand_parent(node);
+        
+        if(!grand_parent) return nullptr;
+        
+        if(grand_parent->left == parent)
+            return grand_parent->right;
+        return grand_parent->left;
+    }
+
 public:
+    RBTree() noexcept : root_(nullptr) {}
+
     void insert(const T& value) override;
     int count_in_range(const T& left, const T& right) const override;
 
